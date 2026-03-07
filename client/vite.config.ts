0ba31e072db377297bdf4b3a52a386e18f3c187d
Simon Hauck <29832799+simonhauck@users.noreply.github.com>
2026-03-07 14:44:47 +0100

    Add tanstack router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react(),
     VitePWA({
       registerType: "autoUpdate",
