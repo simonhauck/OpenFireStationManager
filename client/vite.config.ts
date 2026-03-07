e89a29553d1f1f4fa3edd9aba3e208683753363c
Simon Hauck <29832799+simonhauck@users.noreply.github.com>
2026-03-07 14:27:01 +0100

    Add inital react PWA

---
+++ client/vite.config.ts
@@ -0,0 +1,40 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "OpenFireAnalytics",
+        short_name: "OPA",
+        description:
+          "Manage firefighting stations. Track usage of uniforms, equiements, and much more...",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
